db2e62b2a3dda92b2b94c4453bead1a9961d9cde
pudgefury <jason@techlord.net>
2022-03-31 08:24:26 -0400

    update current supply

--- contracts/CSPN.sol
+++ contracts/CSPN.sol
@@ -8,19 +8,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract CSPN is ERC20, Ownable {
 
     uint private _totalSupply = 13370000;
-    uint private _initialSupply = 1000;
+    uint private _currentSupply = 1000;
     // mapping (address => bool) public _usermint;
 
     constructor() ERC20 ('Crypto Sports Network', 'CSPN') {
-        _mint(owner(), _initialSupply * 10 ** 18);
+        _mint(owner(), _currentSupply * 10 ** 18);
     }
 
     function mint(address to, uint256 amount) external onlyOwner payable {
         require(to != address(0), "CSPN Token: cannot mint to zero address");
         // require(!_usermint[to], 'Already minted');
-        require(_totalSupply > _initialSupply + amount, 'Override totalSupply');
+        require(_totalSupply > _currentSupply + amount, 'Override totalSupply');
         // _usermint[to] = true;
-        _initialSupply = _initialSupply + amount;
+        _currentSupply = _currentSupply + amount;
         _mint(to, amount * 10 ** 18);
         // (bool sent, bytes memory data) = to.call{value: msg.value}("");
         // require(sent, "Failed to send CSPN");
