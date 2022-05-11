743a58c702c1079e506daee9dd5c4b56dc5cac05
pudgefury <jason@techlord.net>
2022-05-11 12:27:01 -0400

    update contract

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 /**
 * @notice Contract is a inheritable smart contract that will add a 
--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
   defaultNetwork: "mumbai",
   etherscan: {
     apiKey: {
