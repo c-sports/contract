1abc032ea09e7ad0d6bb37744b3a8e69510399f5
pudgefury <jasontian28@gmail.com>
2022-07-22 18:23:55 -0400

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   defaultNetwork: "mumbai",
   etherscan: {
     apiKey: {
